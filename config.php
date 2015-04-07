8f4cf0b0fc73952ffdaca1cb541b95db886fcfba
laskalex <alexa.babenco@gmail.com>
2015-04-07 18:11:15 +0300

    2nd try

---
+++ config.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+//хост
+define ("HOST",'localhost');
+//имя пользователя
+define ("USER",'laska');
+//пароль
+define ("PASS",'21731029');
+//имя базы данных
+define ("DB",'data_car');
+//константа твблицы 11
+define ("TABLE1",'categories');
+?>
